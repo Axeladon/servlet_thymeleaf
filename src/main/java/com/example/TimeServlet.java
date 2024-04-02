6d609e1d3a96916ee6b58eed2833f1abc2ef72e4
Axeladon <alexander.developer93@gmail.com>
2024-04-02 22:23:02 +0200

    second commit

--- src/main/java/com/example/TimeServlet.java
+++ src/main/java/com/example/TimeServlet.java
@@ -1,42 +1,71 @@
 package com.example;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.WebApplicationTemplateResolver;
+import org.thymeleaf.web.servlet.JakartaServletWebApplication;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.time.*;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Optional;
 
 @WebServlet("/time")
 public class TimeServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
+    private TemplateEngine engine;
 
-        String param;
-        Map<String, String[]> parameters = req.getParameterMap();
-        if (parameters.isEmpty()) {
-            param = "UTC";
-        } else {
-            param = Util.getAndEncodeRequestParam(req.getParameter("timezone"));
-        }
-        String formattedTime = getFormattedTime(param);
-        showWebPage(out, formattedTime);
+    @Override
+    public void init() {
+        engine = new TemplateEngine();
+
+        JakartaServletWebApplication jswa = JakartaServletWebApplication.buildApplication(this.getServletContext());
+        WebApplicationTemplateResolver resolver = new WebApplicationTemplateResolver(jswa);
+        resolver.setPrefix ("/template/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(engine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        engine.addTemplateResolver(resolver);
     }
 
-    private void showWebPage(PrintWriter out, String formattedTime) {
-        out.println("<html>");
-        out.println("<head><title>Current Time</title></head>");
-        out.println("<body>");
-        out.println("<p>Time: " + formattedTime + "</p>");
-        out.println("</body>");
-        out.println("</html>");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+
+        String param;
+
+        if (req.getParameterMap().isEmpty()) {
+            Optional<Cookie> cookie = Optional.empty();
+            Cookie[] cookies = req.getCookies();
+            if (cookies != null) {
+                cookie = Arrays.stream(cookies)
+                        .filter(c -> "lastTimezone".equals(c.getName()))
+                        .findFirst();
+            }
+
+            if (!cookie.isPresent()) {
+                param = "UTC";
+            } else {
+                param = cookie.get().getValue();
+            }
+        } else {
+            param = Util.getAndEncodeRequestParam(req.getParameter("timezone"));
+            resp.addCookie(new Cookie("lastTimezone", param));
+        }
+
+        String formattedTime = getFormattedTime(param);
+        Context data = new Context(req.getLocale(), Map.of("formattedTime", formattedTime));
+        engine.process("timePage", data, resp.getWriter());
+        resp.getWriter().close();
     }
 
     private String getFormattedTime(String timeZoneStr) {
@@ -60,6 +89,4 @@ public class TimeServlet extends HttpServlet {
             return zonedDateTime.format(formatter) + sign + timeZoneCode;
         }
     }
-
-
 }
